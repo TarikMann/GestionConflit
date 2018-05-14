e304ae9c88675902b288491cde6c1d071db054be
Tarik Mannou <m.tarik.contact@gmail.com>
2018-05-14 12:27:55 +0200

    La branch Master

--- ConflitGit/src/fr/gtm/presentation/Lanceur.java
+++ ConflitGit/src/fr/gtm/presentation/Lanceur.java
@@ -5,7 +5,7 @@ public class Lanceur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello  !!!");
-		System.out.println("Branche Master");
+		System.out.println("La Branch Master");
 	}
 
 }
