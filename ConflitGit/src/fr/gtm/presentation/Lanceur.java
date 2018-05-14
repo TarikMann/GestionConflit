7b64125204f236e6d40e7b822ac4366bee504e2b
Tarik Mannou <m.tarik.contact@gmail.com>
2018-05-14 12:25:30 +0200

    Commit Testeur

--- ConflitGit/src/fr/gtm/presentation/Lanceur.java
+++ ConflitGit/src/fr/gtm/presentation/Lanceur.java
@@ -5,7 +5,7 @@ public class Lanceur {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello  !!!");
-		System.out.println("Branch Testeur");
+		System.out.println("la Branch Testeur ");
 
 	}
 
